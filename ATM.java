43b12f949e1781f6fc2f0414bb567d2ccfbf82d8
asapupravallika <163882439+asapupravallika@users.noreply.github.com>
2024-05-23 14:18:53 +0530

    I developed this StudentGradeCalculator using Java

---
+++ ATM.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+ class Account {
+    private String accountNumber;
+    private double balance;
+
+    public Account(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful!! Current balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount!!");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. Current balance: " + balance);
+        } else {
+            System.out.println("Insufficient balance!!");
+        }
+    }
+}
+
+public class ATM {
+    private Account account;
+    private Scanner scanner;
+
+    public ATM(Account account) {
+        this.account = account;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        while (true) {
+            showMenu();
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    checkBalance();
+                    break;
+                case 2:
+                    deposit();
+                    break;
+                case 3:
+                    withdraw();
+                    break;
+                case 4:
+                    exit();
+                    return;
+                default:
+                    System.out.println("Invalid choice! Please try again!");
+            }
+        }
+    }
+
+    private void showMenu() {
+        System.out.println("\nATM Menu:");
+        System.out.println("1. Check Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Exit");
+        System.out.print("Choose an option: ");
+    }
+
+    private void checkBalance() {
+        System.out.println("Current balance: " + account.getBalance());
+    }
+
+    private void deposit() {
+        System.out.print("Enter deposit amount: ");
+        double amount = scanner.nextDouble();
+        account.deposit(amount);
+    }
+
+    private void withdraw() {
+        System.out.print("Enter withdrawal amount: ");
+        double amount = scanner.nextDouble();
+        account.withdraw(amount);
+    }
+
+    private void exit() {
+        System.out.println("Thank you for using the ATM. Goodbye!");
+    }
+
+    public static void main(String[] args) {
+        Account account = new Account("123456789", 500.0);
+        ATM atm = new ATM(account);
+        atm.start();
+    }
+}
